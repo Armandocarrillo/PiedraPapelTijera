03e6626b392e66855414fc2ba014bf8cc173fbc6
Armandocarrillo <ingcompuarmando@gmail.com>
2019-12-22 21:49:48 -0600

    Initial Commit

---
+++ piedraPapelTijera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piedraPapelTijera
+//
+//  Created by Armando Carrillo on 22/12/19.
+//  Copyright © 2019 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
